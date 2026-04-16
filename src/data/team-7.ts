dd7e43b100337555b42cb3f54bdd832080ad3c55
Danny <dannyyu3512@gmail.com>
2026-04-15 21:00:51 -0700

    Add Danny Yu to team7

--- src/data/team-7.ts
+++ src/data/team-7.ts
@@ -4,9 +4,10 @@ export const team7: Team = {
   name: "Team 7",
   pm: "Danny",
   members: [
-    { name: "", funFact: "" },
-    { name: "", funFact: "" },
-    { name: "", funFact: "" },
-    { name: "", funFact: "" },
+    { name: "Taylor Ueda", funFact: "I am left handed" },
+    { name: "Mira Tian", funFact: "I like drawing" },
+    { name: "Kel Davis", funFact: "I ride motorcycles" },
+    { name: "Joelle Ammari", funFact: "I like rockclimbing!" },
+    { name: "Danishveer Thiara", funFact: "I like movies/animes"}
   ]
 };
